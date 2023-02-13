3393790d8cf7f36a0a44d3cc36aea8bcdff53387
AminMal <amin.malekloo@yahoo.com>
2023-02-13 13:20:53 +0330

    fix: removed the capacity limit from GenerateN

--- generate_n.go
+++ generate_n.go
@@ -2,7 +2,7 @@ package gen
 
 func GenerateN[T any](g Gen[T], n uint) []T {
 	if g == nil { return nil }
-	res := make([]T, n, n)
+	res := make([]T, n)
 	for i := uint(0); i < n; i++ {
 		res[i] = g.Generate()
 	}
